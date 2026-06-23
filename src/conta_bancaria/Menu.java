0e71578ca24f58775880dfcd76afbfdfb8ddfcfa
Dev-Everly <everlysantos98@gmail.com>
2026-06-23 09:46:12 -0300

    :sparkles: Construção da Model Conta Corrente

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -1,5 +1,116 @@
 package conta_bancaria;
 
+import java.util.Scanner;
+
+import conta_bancaria.model.Conta;
+import conta_bancaria.model.ContaCorrente;
+import conta_bancaria.util.Cores;
+
 public class Menu {
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+		int opcao;
+		
+		// Teste da Classe Conta
+		/*
+		 * Conta c1 = new Conta(1, 123, 1, "Adriana Sanches", 10000.0f);
+		 * 
+		 * c1.visualizar();
+		 * 
+		 * c1.setSaldo(15000.0f); c1.setTitular("Maria Joaquina"); c1.visualizar();
+		 * 
+		 * c1.sacar(12000.0f); c1.visualizar();
+		 * 
+		 * c1.depositar(5000.0f); c1.visualizar();
+		 */
+		
+		// Instanciar Objetos da Classe ContaCorrente
+		
+		ContaCorrente cC1 = new ContaCorrente(3, 789, 1, "Raquel", 200000.00f, 20000.00f);
+		cC1.visualizar();
+		
+		System.out.println("\nSacar R$ 203.000,00 da Conta Cc1: " + (cC1.sacar(202000.00f) ?
+				"Saque efetuado com sucesso ! | Saldo: " + cC1.getSaldo() : "Saldo Insuficiente" ));
+		
+		while (true) {
+
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
+					+ "*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("                BANCO DO BRAZIL COM Z                ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Conta                          ");
+			System.out.println("            2 - Listar todas as Contas               ");
+			System.out.println("            3 - Buscar Conta por Numero              ");
+			System.out.println("            4 - Atualizar Dados da Conta             ");
+			System.out.println("            5 - Apagar Conta                         ");
+			System.out.println("            6 - Sacar                                ");
+			System.out.println("            7 - Depositar                            ");
+			System.out.println("            8 - Transferir valores entre Contas      ");
+			System.out.println("            0 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     " + Cores.TEXT_RESET);
+
+			opcao = leia.nextInt();
+
+			if (opcao == 0) {
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				sobre();
+				leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+			case 1:
+				System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
+
+				break;
+			case 2:
+				System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
+
+				break;
+			case 3:
+				System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
+
+				break;
+			case 4:
+				System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
+
+				break;
+			case 5:
+				System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
+
+				break;
+			case 6:
+				System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
+
+				break;
+			case 7:
+				System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
+
+				break;
+			case 8:
+				System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
+
+				break;
+			default:
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+				break;
+			}
+		}
+	}
+
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: Everly");
+		System.out.println("Generation Brasil - generation@generation.org");
+		System.out.println("github.com/Dev-Everly");
+		System.out.println("*********************************************************");
+	}
 
 }
